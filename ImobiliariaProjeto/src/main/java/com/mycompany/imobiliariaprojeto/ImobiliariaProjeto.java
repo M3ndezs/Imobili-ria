e8fe60736f9c4c555d086898bb83e0daf891a3a4
Mendes <162505010+M3ndezs@users.noreply.github.com>
2026-09-23 23:57:40 -0100

    Add files via upload

---
+++ ImobiliariaProjeto/src/main/java/com/mycompany/imobiliariaprojeto/ImobiliariaProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.imobiliariaprojeto;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ImobiliariaProjeto {
+
+    public static void main(String[] args) {
+        
+    }
+}
